ef4ed3ad4a4c81db8127009d9534d0252d8fd99d
ElliotMonde <elliotphua@gmail.com>
2025-02-17 00:47:57 +0800

    fixed errors in TupleDesc and changed Tuple entries to use List instead of Array.

--- src/java/simpledb/storage/Tuple.java
+++ src/java/simpledb/storage/Tuple.java
@@ -1,8 +1,7 @@
 package simpledb.storage;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -14,17 +13,29 @@ public class Tuple implements Serializable {
     private static final long serialVersionUID = 1L;
     private TupleDesc td;
     private RecordId recordId;
+<<<<<<< Updated upstream
+=======
+    private List<Field> entries; // Contains all items within a tuple
+
+>>>>>>> Stashed changes
     /**
      * Create a new tuple with the specified schema (type).
      *
      * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
+     *           the schema of this tuple. It must be a valid TupleDesc
+     *           instance with at least one field.
      */
     public Tuple(TupleDesc td) {
         // some code goes here
+<<<<<<< Updated upstream
         this.td = td;
         this.recordId = null;
+=======
+        this.entries = new ArrayList<>(td.getSize());
+        this.td = td;
+        this.recordId = null;
+
+>>>>>>> Stashed changes
     }
 
     /**
@@ -61,22 +72,35 @@ public class Tuple implements Serializable {
      * Change the value of the ith field of this tuple.
      *
      * @param i
-     *            index of the field to change. It must be a valid index.
+     *          index of the field to change. It must be a valid index.
      * @param f
-     *            new value for the field.
+     *          new value for the field.
      */
+<<<<<<< Updated upstream
     public void setField(int i, Field f) {
         // some code goes here
+=======
+    public void setField(int i, Field f){
+        if (i < this.td.getSize()) {
+            this.entries.set(i, f);
+        }
+>>>>>>> Stashed changes
     }
 
     /**
      * @return the value of the ith field, or null if it has not been set.
      *
      * @param i
-     *            field index to return. Must be a valid index.
+     *          field index to return. Must be a valid index.
      */
     public Field getField(int i) {
+<<<<<<< Updated upstream
         // some code goes here
+=======
+        if (i < this.td.getSize()) {
+            return this.entries.get(i);
+        }
+>>>>>>> Stashed changes
         return null;
     }
 
@@ -88,26 +112,49 @@ public class Tuple implements Serializable {
      *
      * where \t is any whitespace (except a newline)
      */
+    @Override
     public String toString() {
+<<<<<<< Updated upstream
         // some code goes here
         throw new UnsupportedOperationException("Implement this");
+=======
+        String result = "";
+        for (Field entry : entries) {
+            result += entry.toString();
+            result += "\t";
+        }
+        return result.strip();
+>>>>>>> Stashed changes
     }
 
     /**
      * @return
+<<<<<<< Updated upstream
      *        An iterator which iterates over all the fields of this tuple
      * */
     public Iterator<Field> fields()
     {
         // some code goes here
         return null;
+=======
+     *         An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
+        return entries.iterator();
+>>>>>>> Stashed changes
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
+<<<<<<< Updated upstream
      * */
     public void resetTupleDesc(TupleDesc td)
     {
         // some code goes here
+=======
+     */
+    public void resetTupleDesc(TupleDesc td) {
+        this.td = td;
+>>>>>>> Stashed changes
     }
 }
